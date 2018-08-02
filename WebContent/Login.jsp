698073fa5c86a41b39d4f4bae8c0bf72d23a755d
Sneha.Kanthak <Sneha.Kanthak@SKANTHAK01.corp.neudesic.net>
2018-08-02 15:33:52 +0530

    Sample Cloud For jenkins

---
+++ WebContent/Login.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+Welcome.. Please enter your name....
+<form action = WelcomeServlet method = post>
+Name :<input type = "text" name = "userName"> <br> <br>
+		<input type = "submit" name = "submit" value = "Submit">
+</form>
+</body>
+</html>
---
+++ src/com/sample/example/WelcomeServlet.java
@@ -0,0 +1,47 @@
+package com.sample.example;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		String userName = request.getParameter("userName");
+		RequestDispatcher rd = request.getRequestDispatcher("Welcome.jsp");
+		rd.include(request, response);
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
